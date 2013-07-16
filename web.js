f25090b73dcb644fde6dfb73e54730cd6add3ebd
IG <ingridgabor@gmail.com>
2013-07-16 23:15:45 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var content = fs.readFileSync('index.html', 'utf8');
+  var buf = new Buffer(fs.readFileSync('index.html'), 'utf8');
+  response.send(buf.toString()); 
 });
 
 var port = process.env.PORT || 5000;
